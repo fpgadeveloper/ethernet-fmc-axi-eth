aef7decc1c6f4921377810b8c08ab6eba760414a
Jeff Johnson <jeff@fpgadeveloper.com>
2015-10-07 21:58:56 -0400

    fixed implementation commands

--- Vivado/build-vc709.tcl
+++ Vivado/build-vc709.tcl
@@ -102,7 +102,6 @@ make_wrapper -files [get_files $design_name.bd] -top -import
 if {[lindex $argv 0] == "bitstream"} {
   launch_runs synth_1
   wait_on_run synth_1
-  launch_runs impl_1
-  wait_on_run impl_1
   launch_runs impl_1 -to_step write_bitstream
+  wait_on_run impl_1
 }
